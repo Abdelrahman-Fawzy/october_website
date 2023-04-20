a6c0445ff4961f405762753b82b70f28a749212f
Abdelrahman-Fawzy <Abdelrahmanawzy98@gmail.com>
2023-04-20 17:09:28 +0200

    finish responisive

--- src/app/study-majors/study-majors.component.ts
+++ src/app/study-majors/study-majors.component.ts
@@ -20,23 +20,23 @@ export class StudyMajorsComponent {
   register() {
     this.modalRef = this.modalService.show(RegisterModalComponent, {class: 'modal-xl modal-dialog-centered'});
   }
-  @HostListener('window:scroll', ['$event']) // for window scroll events
+  // @HostListener('window:scroll', ['$event']) // for window scroll events
   // bodyHight
 
-  onScroll(event) {
-    let majorsSections = document.getElementById('majors-section')
-    let majorsAside = document.getElementById('majorsAside')
-    let bodyHeight = window.pageYOffset
-    if (bodyHeight >= 1500) {
-      majorsAside.style.position = 'relative'
-      majorsAside.style.height = 'auto'
-      // majorsSections.style.marginRight = '0px'
-    } else {
-      majorsAside.style.position = 'fixed'
-      majorsAside.style.height = '75vh'
-      // majorsSections.style.marginRight = '350px'
-    }
-  }
+  // onScroll(event) {
+  //   let majorsSections = document.getElementById('majors-section')
+  //   let majorsAside = document.getElementById('majorsAside')
+  //   let bodyHeight = window.pageYOffset
+  //   if (bodyHeight >= 1500) {
+  //     majorsAside.style.position = 'relative'
+  //     majorsAside.style.height = 'auto'
+  //     majorsSections.style.marginRight = '0px'
+  //   } else {
+  //     majorsAside.style.position = 'fixed'
+  //     majorsAside.style.height = '75vh'
+  //     majorsSections.style.marginRight = '350px'
+  //   }
+  // }
 
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template, this.config);
